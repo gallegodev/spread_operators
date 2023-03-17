d15f3904337525e1799a999508b7bece9377690b
gallegodev <gallegodev1991@gmail.com>
2023-03-17 16:10:03 +0000

    Add rest function to lesson content

--- the-spread-operator.js
+++ the-spread-operator.js
@@ -20,3 +20,16 @@ let obj2 = { ...obj1, d: 4 };
 // Copying only part of an array/object
 let arr5 = [...arr1, { ...obj1 }, ...arr3, "x", "y", "z"];
 console.log(arr5);
+
+// Function using ...rest
+const sumRest = (a, b, c, ...rest) => {
+    let sum = a + b + c;
+    for (let i of rest){
+        sum += i;
+    }
+
+    return sum;
+}
+
+let sum3 = sumRest(1, 2, 3, 4, 5, 6);
+console.log("Sum3:",sum3);
